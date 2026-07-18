84b847539095157a8b9e6c7bca4ce6ba2e25c13d
Siddik Imran Gadkari <gadakarisiddhik@gmail.com>
2026-07-18 20:39:53 +0530

    Initial project structure

---
+++ Lecture2.py
@@ -0,0 +1,15 @@
+#slicing index in string--
+# str = "Siddik_Gadkari"
+# print(str[1:4]) #o/p = idd
+# print(str[7:14]) #o/p = Gadkari
+# print(str[0:len(str)]) #o/p = Siddik_Gadkari
+
+#Negative Indexing Slicing--
+# str = "Apple_Mango"
+# print(str[-11:-6]) #o/p = Apple 
+
+#String Functions--
+str = "i am a coder."
+print(str.endswith("er")) #o/p = flase
+print(str.capitalize()) #o/p = I am a coder.
+print(str.replace("a","G")) #o/p = i Gm G coder.
