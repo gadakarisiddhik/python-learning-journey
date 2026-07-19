bee279237295435341f733f15ecf115692ff5c09
Siddik Imran Gadkari <gadakarisiddhik@gmail.com>
2026-07-19 09:45:47 +0530

    Added python Arithmetic Operato examples

---
+++ 02_Operators/arithmetic_operators.py
@@ -0,0 +1,16 @@
+"""
+Topic: Arithmetic Operators
+Author: Siddik Imran Gadkari
+Date: 19 July 2026
+
+Description:
+This program demonstrates the use of Arithmetic Operator in Python.
+"""
+a = 5
+b = 2
+print(a+b) #addition 
+print(a-b) #substraction
+print(a*b) #multiplication
+print(a/b) #division
+print(a%b) #modulo
+print(a**b) #power of a^b 
