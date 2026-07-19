c1f8c0569157f427b28f60d264c280b40a66957f
Siddik Imran Gadkari <gadakarisiddhik@gmail.com>
2026-07-19 09:18:03 +0530

    Added python comments example

---
+++ 01_Basics/comments.py
@@ -0,0 +1,12 @@
+#Topic : Comments 
+#Author : Siddhik Imran Gadakari
+#Description : This program demonstrates single-line and multi-line comments in Python.
+
+#<------ Used for Single Line 
+#   """<------ Used for Multiline Line 
+
+"""
+This is a multi-line comment.
+It is used to explain
+multiple lines of code.
+"""
