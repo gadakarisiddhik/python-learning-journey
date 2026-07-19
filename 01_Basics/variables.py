31a4588b869888f6382eed08cd341cc6acb0a6b8
Siddik Imran Gadkari <gadakarisiddhik@gmail.com>
2026-07-19 09:26:51 +0530

    Added python variables examples

---
+++ 01_Basics/variables.py
@@ -0,0 +1,26 @@
+"""
+Topic: Variables
+Author: Siddik Imran Gadkari
+Date: 19 July 2026
+
+Description:
+This program demonstrates the use of variables in Python.
+"""
+
+# String Variable
+name = "Siddik"
+
+# Integer Variable
+age = 20
+
+# Float Variable
+height = 5.6
+
+# Boolean Variable
+is_student = True
+
+# Display Output
+print("Name :", name)
+print("Age :", age)
+print("Height :", height)
+print("Student :", is_student)
