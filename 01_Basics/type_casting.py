c18e7275800376e7d05acb505f184b52f579f357
Siddik Imran Gadkari <gadakarisiddhik@gmail.com>
2026-07-19 17:57:31 +0530

    Added Python Typecasting example

--- 01_Basics/type_casting.py
+++ 01_Basics/type_casting.py
@@ -4,7 +4,7 @@ Author: Siddik Imran Gadkari
 Date: 19 July 2026
 
 Description:
-This program demonstrates the use of Typecast in Python.
+This program demonstrates the use of ypecast in Python.
 """
 
 #type casting 
@@ -17,5 +17,10 @@ print(type(b)) # B is int datatype
 
 c = int(a) # A string value typecast by int C value 
 print(type(c)) #o/p: INT
-
 print(c+b) #addition of INT and String Datatype
+
+d = float("5.2")
+print(type(d),d) # string to float
+
+e = str(125)
+print(type(e),e) #int to string
