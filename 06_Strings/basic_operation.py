76ee31c6164b0f48214b1634933c9dc0af0dfd40
Siddik Imran Gadkari <gadakarisiddhik@gmail.com>
2026-07-19 18:28:42 +0530

    Added Python String Basic Operation examples

---
+++ 06_Strings/basic_operation.py
@@ -0,0 +1,33 @@
+"""
+Topic: String Basic Operation 
+Author: Siddik Imran Gadkari
+Date: 19 July 2026
+
+Description:
+This program demonstrates the use of String Basic Operation in Python.
+"""
+
+#creating String 
+str1 = "Siddik Imran Gadkari"
+str2 = "My Name is Siddik"
+str3 = "HellowWorld"
+
+#concatenation of string using "+" Charector
+s = "Siddik"
+i = "Imran"
+g = "Gadkari"
+fullname = s+" "+i+" "+g
+print("Name:",fullname)
+
+#Length of Sting using len()
+name = "Shradha"
+str = len(name)
+print("Length:",str)
+
+#Indexing of string 
+ #String - S    i    d    d    i    k
+#indexing- 0    1    2    3    4    5
+
+str4 = "Samay Raina"
+char = str4[0]
+print(char) #o/p: S
