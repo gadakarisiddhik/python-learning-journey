66f0106ac2fd1e0c84689d1ddeb2b4b89d52ec67
Siddik Imran Gadkari <gadakarisiddhik@gmail.com>
2026-07-19 13:39:29 +0530

    Added python Comparison Operator Examples

---
+++ 02_Operators/comparison_operators.py
@@ -0,0 +1,16 @@
+"""
+Topic: Comparison Operator
+Author: Siddik Imran Gadkari
+Date: 19 July 2026
+
+Description:
+This program demonstrates the use of Comparison Operator in Python.
+"""
+a = 50
+b = 20
+print(a==b) #check a equals to b and return true or false ,o/p: flase
+print(a!=b) #check a not equals to b and return true or false ,o/p: true
+print(a>b) #check greater than and return true or false ,o/p: true
+print(a>=b) #check greater than equal to and return true or false ,o/p: true
+print(a<b) #check less than and return true or false ,o/p: flase
+print(a<=b) #check less than equal to and return true or false ,,o/p: flase
