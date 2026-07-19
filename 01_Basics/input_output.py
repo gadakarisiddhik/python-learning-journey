15a5dc10cca2fc0bdb84f6c48b9ffde310dbf7c2
Siddik Imran Gadkari <gadakarisiddhik@gmail.com>
2026-07-19 09:24:43 +0530

    Added python input and output example

---
+++ 01_Basics/input_output.py
@@ -0,0 +1,28 @@
+"""
+Topic: Input and Output
+Author: Siddik Imran Gadkari
+Date: 19 July 2026
+
+Description:
+This program demonstrates how to take input from the user
+and display output in Python.
+"""
+
+# Output
+print("Welcome to Python!")
+
+# Input
+name = input("Enter your name: ")
+
+# Output using input
+print("Hello,", name)
+
+# Taking integer input
+age = int(input("Enter your age: "))
+
+print("Your age is", age)
+
+# Taking float input
+height = float(input("Enter your height in feet: "))
+
+print("Your height is", height, "feet")
