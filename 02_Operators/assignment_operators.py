7166c093b9eca57ce3f0a5cd0774cd525a4fe115
Siddik Imran Gadkari <gadakarisiddhik@gmail.com>
2026-07-19 13:54:24 +0530

    Added Python Assignment Operators Examples

---
+++ 02_Operators/assignment_operators.py
@@ -0,0 +1,29 @@
+"""
+Topic: Assignment Operators
+Author: Siddik Imran Gadkari
+Date: 19 July 2026
+
+Description:
+This program demonstrates the use of Assignment Operators in Python.
+"""
+
+a = 10 #10 values assign to the a variable
+print("Origenal Value: ",a)
+
+a += 1 #addition assignment
+print(a)
+
+a -= 5 #subtraction assignment
+print(a)
+
+a *= 2 #multiplication assignment
+print(a)
+
+a /= 6 #division assignment
+print(a)
+
+a %= 5 #modulus assignment
+print(a)
+
+a **= 2 #power assignment
+print(a)
