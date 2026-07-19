3dd4445c3152b907f3d68ec43a4f268cd0a43f52
Siddik Imran Gadkari <gadakarisiddhik@gmail.com>
2026-07-19 17:48:44 +0530

    Added Python typecast example

---
+++ 01_Basics/type_casting.py
@@ -0,0 +1,21 @@
+"""
+Topic: Typecast
+Author: Siddik Imran Gadkari
+Date: 19 July 2026
+
+Description:
+This program demonstrates the use of Typecast in Python.
+"""
+
+#type casting 
+
+a = "2"
+print(type(a)) # A is string datatype
+
+b = 13
+print(type(b)) # B is int datatype
+
+c = int(a) # A string value typecast by int C value 
+print(type(c)) #o/p: INT
+
+print(c+b) #addition of INT and String Datatype
