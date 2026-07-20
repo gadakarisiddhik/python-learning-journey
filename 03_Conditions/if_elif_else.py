387cd19c28099a47d103b7044986a29a5b479825
Siddik Imran Gadkari <gadakarisiddhik@gmail.com>
2026-07-20 20:21:19 +0530

    Adde Python if elif else Example

---
+++ 03_Conditions/if_elif_else.py
@@ -0,0 +1,23 @@
+"""
+Topic: If Elif Else
+Author: Siddik Imran Gadkari
+Date: 19 July 2026
+
+Description:
+This program demonstrates the use of Conditional Statement in Python.
+"""
+#if Statement 
+age = 18
+if(age >= 18): #check condition and take decision
+    print("Your can vote")
+
+#if-elif-esle
+light = "green"
+if (light=="red"):
+    print("Stop")
+elif(light=="green"): #elif check ondition whenever if condition is false
+    print("Go")
+elif(light=="yellow"):
+    print("Look")
+else:                 #else alwayes run whenever if and elif condtions are false
+    print("Wrong Input")
