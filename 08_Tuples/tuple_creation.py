8419fbb8636946e8d14505658e7712fbb101f23c
Siddik Imran Gadkari <gadakarisiddhik@gmail.com>
2026-07-21 12:41:08 +0530

    Added Python Tuple Creation Examples

---
+++ 08_Tuples/tuple_creation.py
@@ -0,0 +1,20 @@
+"""
+Topic: Tuple Creaton
+Author: Siddik Imran Gadkari
+Date: 21 July 2026
+
+Description:
+This program demonstrates the Tuple Creation in Python.
+"""
+
+"""
+Tuple is the built in datatype in which store values like list,
+tuple is immutable datatype.
+"""
+tup = (88,72,62,14,93,1,46) #create Tuple using "()" 
+print(type(tup)) #o/p: <class 'tuple'>
+print(tup) #o/p: (88, 72, 62, 14, 93, 1, 46)
+
+tup2 = ("Siddik",20,"Samarth",21,"Aditya","22")
+print(type(tup2)) #o/p: <class 'tuple'>
+print(tup2) #o/p: ('Siddik', 20, 'Samarth', 21, 'Aditya', '22')
