d4b2528801d7bc8685085ab04711653fcd939319
Siddik Imran Gadkari <gadakarisiddhik@gmail.com>
2026-07-21 12:52:29 +0530

    Added Python Tuple Indexing  Examples

---
+++ 08_Tuples/tuple_indexing.py
@@ -0,0 +1,16 @@
+"""
+Topic: Tuple Indexing
+Author: Siddik Imran Gadkari
+Date: 21 July 2026
+
+Description:
+This program demonstrates the use of Tuple Indexing in Python.
+"""
+
+tuple = ("Joy",72,"Jack",46,"Adam",99,"Rosy",92) #Tuple Indexing Start With 0 
+
+print(tuple[0]) #o/p: ("Joy")
+print(tuple[2]) #o/p: ("Jack")
+print(tuple[7]) #o/p: ("92")
+
+# tuple[0] = 7 , 'tuple' object does not support item assignment
